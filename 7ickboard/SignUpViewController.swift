28db499b63fe03b2579ce97d91ccd3a8fd621e60
Bread-kn72 <ordinarytom72@gmail.com>
2024-04-22 20:22:38 +0900

    Add : UI추가 및 키보드 내리기 구현

---
+++ 7ickboard/SignUpViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.swift
+//  7ickboard
+//
+//  Created by Kinam on 4/22/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+}
---
+++ 7ickboard/UserModel.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  7ickboard
+//
+//  Created by Kinam on 4/22/24.
+//
+
+import UIKit
+
+final class UserModel {
+    struct User {
+        var email: String
+        var password: String
+        var name: String
+        var telephone: String
+        var driversLicense: Bool
+    }
+    
+    var users: [User] = [
+        User(email: "test1234", password: "test1234", name: "test" , telephone: "010-1234-5678", driversLicense: true),
+    ]
+    
+    // 비밀번호 형식 검사
+    func isValidPassword(pwd: String) -> Bool {
+        let passwordRegEx = "^[a-zA-Z0-9]{8,16}$"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        return passwordTest.evaluate(with: pwd)
+    }
+    
+    // 전화번호 형식 검사
+    func isValidTelephone(phone: String) -> Bool {
+        let phoneRegEx = "^010\\d{8}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegEx)
+        return phoneTest.evaluate(with: phone)
+    }
+}
