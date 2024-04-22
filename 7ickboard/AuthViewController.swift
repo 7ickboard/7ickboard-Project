3ad11a84f02988b8b68dccfb7931d39eb4554b08
Bread-kn72 <ordinarytom72@gmail.com>
2024-04-22 16:22:35 +0900

    Add : 로그인, 회원가입 UI 구현

---
+++ 7ickboard/AuthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.swift
+//  7ickboard
+//
+//  Created by Kinam on 4/22/24.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
