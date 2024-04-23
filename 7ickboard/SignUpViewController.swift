b93799d9c59be045ceae4d6b9cddaa82f365110d
Bread-kn72 <ordinarytom72@gmail.com>
2024-04-23 11:37:26 +0900

    Add : 회원가입 공란 예외처리

--- 7ickboard/SignUpViewController.swift
+++ 7ickboard/SignUpViewController.swift
@@ -37,7 +37,7 @@ class SignUpViewController: UIViewController {
     }
     
     func setSegmentedControl() {
-        licenseSegmentedControl.selectedSegmentIndex = 0
+        licenseSegmentedControl.selectedSegmentIndex = 1
         licenseSegmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
     }
     
@@ -46,19 +46,32 @@ class SignUpViewController: UIViewController {
         print("Is Licensed: \(isLicensed)")
     }
     
-    @IBAction func signupButton(_ sender: Any) {
-        guard let id = idTextField.text else {
+    @IBAction func signupButton(_ sender: UIButton) {
+        guard let id = idTextField.text, !id.isEmpty else {
+            makeAlert(for: "id")
             return
         }
-        guard let pw = passwordTextField.text else {
+        guard let pw = passwordTextField.text, !pw.isEmpty else {
+            makeAlert(for: "비밀번호")
             return
         }
-        guard let name = nameTextField.text else {
+        guard let name = nameTextField.text, !name.isEmpty else {
+            makeAlert(for: "이름")
             return
         }
-        guard let tp = telephoneTextField.text else {
+        guard let tp = telephoneTextField.text, !name.isEmpty else {
+            makeAlert(for: "휴대폰번호")
             return
         }
+        
+    }
+    
+    func makeAlert(for item: String) {
+        print("회원가입 실패")
+        let alert = UIAlertController(title: "회원가입 실패", message: "\(item)이(가) 없습니다.", preferredStyle: .alert)
+        let okAlert = UIAlertAction(title: "확인", style: .cancel)
+        alert.addAction(okAlert)
+        present(alert, animated: true, completion: nil)
         
     }
 }
