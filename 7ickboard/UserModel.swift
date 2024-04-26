8305e9bae4b004b51da18e82436e244ccb70d267
Bread-kn72 <ordinarytom72@gmail.com>
2024-04-26 15:20:23 +0900

    Fix : static 접근으로 변경

--- 7ickboard/UserModel.swift
+++ 7ickboard/UserModel.swift
@@ -34,109 +34,65 @@ final class UserModel {
             self.startTime = startTime
             self.endTime = endTime
         }
-        
-        func formattedTime() -> (String, String) {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "HH:mm"
-            
-            let startTimeString = startTime.map { formatter.string(from: $0) } ?? "00:00"
-            let endTimeString = endTime.map { formatter.string(from: $0) } ?? "00:00"
-            
-            return (startTimeString, endTimeString)
-        }
     }
-}
+    
+    static var users: [User] = []
 
-static var users: [User] = []
-
-init() {
-    // UserDefaults에서 저장된 사용자 데이터를 불러오기
-    if let userData = UserDefaults.standard.data(forKey: "userData") {
-        if let decodedData = try? JSONDecoder().decode([User].self, from: userData) {
-            <<<<<<< HEAD
-            self.users = decodedData
-            =======
-            UserModel.users = decodedData
-            >>>>>>> develop
-            return
+    init() {
+        // UserDefaults에서 저장된 사용자 데이터를 불러오기
+        if let userData = UserDefaults.standard.data(forKey: "userData") {
+            if let decodedData = try? JSONDecoder().decode([User].self, from: userData) {
+                UserModel.users = decodedData
+                return
+            }
         }
-    }
-    // 저장된 사용자 데이터가 없을 경우, 기본값 설정
-    self.users = [
+        // 저장된 사용자 데이터가 없을 경우, 기본값 설정
         UserModel.users = [
             User(id: "test1234", password: "test1234", name: "test" , telephone: "010-1234-5678", driversLicense: true)
         ]
-        }
-        
-        // 비밀번호 형식 검사
-        func isValidPassword(pwd: String) -> Bool {
-            let passwordRegEx = "^[a-zA-Z0-9]{8,16}$"
-            let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
-            return passwordTest.evaluate(with: pwd)
-        }
-        
-        // 전화번호 형식 검사
-        func isValidTelephone(phone: String) -> Bool {
-            let phoneRegEx = "^010\\d{8}$"
-            let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegEx)
-            return phoneTest.evaluate(with: phone)
-        }
-        
-        // 회원 추가
-        func addUser(user: User) {
-            <<<<<<< HEAD
-            users.append(user)
-            save() // 변경사항을 저장
-        }
-        
-        // 사용자 데이터를 UserDefaults에 저장
-        func save() {
-            if let encodedData = try? JSONEncoder().encode(users) {
-                UserDefaults.standard.set(encodedData, forKey: "userData")
-            }
-        }
-        
-        func removeUser(withId id: String) {
-            users = users.filter { $0.id != id }
-            save() // 변경사항을 저장
-        }
-        
-        func removeAllUsers() {
-            users = []
-            save() // 변경사항을 저장
-        }
-        
-        func updateRidingTime(forUserId userId: String, startTime: Date?, endTime: Date?) {
-            if let index = users.firstIndex(where: { $0.id == userId }) {
-                users[index].ridingTime = RidingTime(startTime: startTime, endTime: endTime)
-                save()
-            }
-        }
+    }
+    
+    // 비밀번호 형식 검사
+    func isValidPassword(pwd: String) -> Bool {
+        let passwordRegEx = "^[a-zA-Z0-9]{8,16}$"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        return passwordTest.evaluate(with: pwd)
+    }
+    
+    // 전화번호 형식 검사
+    func isValidTelephone(phone: String) -> Bool {
+        let phoneRegEx = "^010\\d{8}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegEx)
+        return phoneTest.evaluate(with: phone)
+    }
+    
+    // 회원 추가
+    func addUser(user: User) {
         UserModel.users.append(user)
         UserModel.save() // 변경사항을 저장
+    }
+    
+    // 사용자 데이터를 UserDefaults에 저장
+    static func save() {
+        if let encodedData = try? JSONEncoder().encode(UserModel.users) {
+            UserDefaults.standard.set(encodedData, forKey: "userData")
         }
-        
-        // 사용자 데이터를 UserDefaults에 저장
-        static func save() {
-            if let encodedData = try? JSONEncoder().encode(UserModel.users) {
-                UserDefaults.standard.set(encodedData, forKey: "userData")
-            }
+    }
+
+    static func updateRidingTime(forUserId userId: String, startTime: Date?, endTime: Date?) {
+        if let index = UserModel.users.firstIndex(where: { $0.id == userId }) {
+            UserModel.users[index].ridingTime = RidingTime(startTime: startTime, endTime: endTime)
+            save()
         }
-        
-        static func updateRidingTime(forUserId userId: String, startTime: Date?, endTime: Date?) {
-            if let index = UserModel.users.firstIndex(where: { $0.id == userId }) {
-                UserModel.users[index].ridingTime = RidingTime(startTime: startTime, endTime: endTime)
-                save()
-            }
-        }
-        
-        func removeUser(withId id: String) {
-            UserModel.users = UserModel.users.filter { $0.id != id }
-            UserModel.save() // 변경사항을 저장
-        }
-        
-        func removeAllUsers() {
-            UserModel.users = []
-            UserModel.save() // 변경사항을 저장
-        }
-        }
+    }
+
+    func removeUser(withId id: String) {
+        UserModel.users = UserModel.users.filter { $0.id != id }
+        UserModel.save() // 변경사항을 저장
+    }
+    
+    func removeAllUsers() {
+        UserModel.users = []
+        UserModel.save() // 변경사항을 저장
+    }
+}
